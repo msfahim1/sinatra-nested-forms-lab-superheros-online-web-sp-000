0ae5ba2ca70b93be9e2c5cd762b79dbb3f6cb831
Michael Fahim <michael_fahim@yahoo.com>
2020-08-05 23:57:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      params[:team][:heroes].map! do |m_data|
+        Hero.from_params(m_data)
+      end
+      @team = Team.from_params(params[:team])
+      erb :team
+    end
 end
